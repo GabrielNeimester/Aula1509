cae47fcb08f39b107fba86c977dd24b0ff8ca9f5
Gabriel Neimester <gneimester@hotmail.com>
2023-09-22 20:03:33 -0300

    testando

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import ButtonFatec from "../../components/ButtonFatec";
 import InputFatec from "../../components/InputFatec";
 
@@ -9,6 +10,8 @@ export default function Home() {//precisa ter letra maiuscula no nome da functio
       <InputFatec type='text' placeholder='Digite seu E-mail' value='teste@email.com'/>
       <InputFatec type='text' placeholder='Digite seu Telefone' value='1234578910'/>
       <ButtonFatec type='submit' label='Enviar'/>
+      <hr />
+      <Link to='/sobre'>Sobre</Link>
     </>
   )
 }
--- src/pages/sobre/index.tsx
+++ src/pages/sobre/index.tsx
@@ -1,9 +1,11 @@
+import { Link } from "react-router-dom";
+
 export default function Sobre(){
     return(
     <>
     <h1>Sobre</h1>
     <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corporis nisi ipsam sint maiores voluptas deserunt expedita consequuntur error, quia, molestias amet, veniam totam ad perferendis quaerat sapiente voluptates veritatis sunt.</p>
-
+    <Link to='/'>Home</Link>
     </>
     )
 }
